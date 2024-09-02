b629302ce068ce14d156aef820f6d520c8490734
euneun9 <eej3652@gmail.com>
2024-09-02 16:47:54 +0900

    day4

--- fisa_django/account/urls.py
+++ fisa_django/account/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', views.user_login),
+    path('login/', auth_views.LoginView.as_view()),
+    path('logout/', auth_views.LogoutView.as_view())
 ]
--- fisa_django/fisa_django/urls.py
+++ fisa_django/fisa_django/urls.py
@@ -20,14 +20,15 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
-
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("blog/", include('blog.urls') ),
-    path("account/", include('account.urls')),
-]
-
+    # path("account/", include('account.urls')),
+    path('accounts/', include('allauth.urls')),
+    path("", include('blog.urls')),
+] + debug_toolbar_urls()
 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
