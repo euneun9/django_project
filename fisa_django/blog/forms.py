7f2a2c252384cc12005bf769fb61645e74572d61
euneun9 <eej3652@gmail.com>
2024-09-03 12:38:08 +0900

    migrate db

--- fisa_django/blog/forms.py
+++ fisa_django/blog/forms.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
-from .models import Comment, Post, Tag
+from .models import Comment
+from django import forms
 
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Tag)
+# 커스텀한 폼을 사용하기 위한 클래스
+class CommentForm(forms.ModelForm):
+    class Meta:  # 추가적인 정보들을 달아서 보낼 때 사용하는 클래스
+        model = Comment
+        fields = ['content']
--- fisa_django/fisa_django/settings.py
+++ fisa_django/fisa_django/settings.py
@@ -12,7 +12,20 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from django.utils import timezone
-import os
+
+from dotenv import load_dotenv
+import os 
+
+# load .env
+load_dotenv()
+MYENGINE = os.environ.get('MYENGINE')
+MYNAME = os.environ.get('MYNAME')
+MYUSER = os.environ.get('MYUSER')
+MYPW = os.environ.get('MYPW')
+MYHOST = os.environ.get('MYHOST')
+MYPORT = os.environ.get('MYPORT')
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,11 +106,27 @@ AUTHENTICATION_BACKENDS = [
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": MYENGINE,
+        "NAME": MYNAME, # DB Name(스키마랑 같은 이름)
+        "USER": MYUSER,
+        "PASSWORD": MYPW,
+        "HOST": MYHOST,
+        "PORT": MYPORT,
+    },
+    'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'" #  MySQL 데이터베이스의 초기 설정 명령어
+        },                                                       # STRICT_TRANS_TABLES: 트랜잭션을 지원하는 테이블, 
+                                                                  # 잘못된 데이터가 삽입되거나 업데이트될 때 경고 대신 오류를 발생
 }
 
 
